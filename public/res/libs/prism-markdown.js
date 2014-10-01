fa659e7d146f9056ebe8d2b0771ff6c33d555edf
Alireza Nourian <az.nourian@gmail.com>
2014-10-01 17:31:28 +0330

    refine prism files

--- public/res/libs/prism-markdown.js
+++ public/res/libs/prism-markdown.js
@@ -1,3 +1,16 @@
+Prism.languages.latex = {
+    // A tex command e.g. \foo
+	'keyword': /\\(?:[^a-zA-Z]|[a-zA-Z]+)/g,
+    // Curly and square braces
+	'lparen': /[[({]/g,
+    // Curly and square braces
+	'rparen': /[\])}]/g,
+    // A comment. Tex comments start with % and go to
+    // the end of the line
+	'comment': /%.*/g,
+};
+
+
 Prism.languages.md = (function() {
 
 	var charInsideUrl = "[-A-Z0-9+&@#/%?=~_|[\\]()!:,.;]",
